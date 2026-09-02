7d0ebfeb83853482ccdb8c60b82f8dd236700823
Mi-no-Kim <71382553+Mi-no-Kim@users.noreply.github.com>
2026-09-02 18:16:04 +0900

    add spring project

---
+++ src/main/java/com/example/codeownerslab/CodeownersLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeownerslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeownersLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeownersLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeownerslab/CodeownersLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeownerslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeownersLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
